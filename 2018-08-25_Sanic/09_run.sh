881c83e8b00b90acf7e4182fd0d934e795e45c27
Danilo J. S. Bellini <danilo.bellini@gmail.com>
2018-08-29 00:01:25 -0300

    Avoid sending a payload to a GET in timer script
    That's a complete rewrite of the script, it can perhaps be seen as a
    simplification, but the shells got more nested

--- 2018-08-25_Sanic/09_run.sh
+++ 2018-08-25_Sanic/09_run.sh
@@ -1,4 +1,2 @@
-time sh -c ' \
-  seq 10 \
-  | xargs -n1 -P5 curl -sXGET "localhost:1337?input=flaskconf" -d \
-  | jq -c'
+command='curl -s "localhost:1337?input=flaskconf"'
+time sh -c "seq 10 | sed 'c $command' | xargs -d'\n' -n1 -P5 sh -c | jq -c"
